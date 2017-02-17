93c114d9dbfe665b1acd86c62b18bcafa59b2fd3
Teclab85 <teclab85@gmail.com>
2017-02-17 13:21:33 -0500

    Current address is displayed now

--- app/src/main/java/trace/traceapp/GPSHandler.java
+++ app/src/main/java/trace/traceapp/GPSHandler.java
@@ -60,8 +60,8 @@ class GPSHandler {
     }
     protected void createLocationRequest() {
         LocationRequest mLocationRequest = new LocationRequest();
-        mLocationRequest.setInterval(10000);
-        mLocationRequest.setFastestInterval(5000);
+        mLocationRequest.setInterval(3600000);
+        mLocationRequest.setFastestInterval(60000);
         mLocationRequest.setPriority(LocationRequest.PRIORITY_HIGH_ACCURACY);
     }
     String getLatitude() {
