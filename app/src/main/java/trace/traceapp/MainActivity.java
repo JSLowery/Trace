a402ca47c6d3070877eeb59652fbe0e989c86942
David Long <david.c.long3@gmail.com>
2017-02-22 20:55:11 -0500

    I changed the names of the menu buttons. That is all

--- app/src/main/java/trace/traceapp/MainActivity.java
+++ app/src/main/java/trace/traceapp/MainActivity.java
@@ -160,13 +160,9 @@ public class MainActivity extends AppCompatActivity
 
         if (id == R.id.nav_stats) {
             // Handle the camera action
-        } else if (id == R.id.nav_gallery) {
+        } else if (id == R.id.nav_mapview) {
 
-        } else if (id == R.id.nav_slideshow) {
-
-        } else if (id == R.id.nav_manage) {
-
-        } else if (id == R.id.nav_share) {
+        }  else if (id == R.id.nav_share) {
 
         } else if (id == R.id.nav_savecanvas) {
 
