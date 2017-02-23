5d8803912c3fd11daa08b8ad1e5797e878a16d1b
David Long <david.c.long3@gmail.com>
2017-02-22 20:22:48 -0500

    I changed the names of the menu buttons. That is all

--- app/src/main/java/trace/traceapp/MainActivity.java
+++ app/src/main/java/trace/traceapp/MainActivity.java
@@ -142,9 +142,9 @@ public class MainActivity extends AppCompatActivity
         // Handle navigation view item clicks here.
         int id = item.getItemId();
 
-        if (id == R.id.nav_camera) {
+        if (id == R.id.nav_stats) {
             // Handle the camera action
-        } else if (id == R.id.nav_gallery) {
+        } else if (id == R.id.nav_mapview) {
             //GPSHandler appLocationManager = new GPSHandler(MainActivity.this);
             appLocationManager.getLatitude();
             appLocationManager.getLongitude();
@@ -157,13 +157,9 @@ public class MainActivity extends AppCompatActivity
             txt.setText("Lattitude: "+appLocationManager.getLatitude()+ " Longetude:"+ appLocationManager.getLongitude());
            // mLatitudeTextView.setText(String.format("%s: %f", mLatitudeLabel,
               //      mCurrentLocation.getLatitude()));
-        } else if (id == R.id.nav_slideshow) {
+        }  else if (id == R.id.nav_share) {
 
-        } else if (id == R.id.nav_manage) {
-
-        } else if (id == R.id.nav_share) {
-
-        } else if (id == R.id.nav_send) {
+        } else if (id == R.id.nav_savecanvas) {
 
         }
 
