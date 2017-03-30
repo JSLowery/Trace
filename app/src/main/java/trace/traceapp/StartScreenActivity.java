c819a3d30396925aebd27fc846a19edf6e5d2289
aduffyguy <aduffyguy@gmail.com>
2017-03-30 14:14:07 -0400

    Start Screen Activity -Adds the "start Screen" where the user sets their name.   -Connectitvity to the main page is set up.   -Saving name to internal device memory will come next update.

--- app/src/main/java/trace/traceapp/StartScreenActivity.java
+++ app/src/main/java/trace/traceapp/StartScreenActivity.java
@@ -2,12 +2,34 @@ package trace.traceapp;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.content.Intent;
 
 public class StartScreenActivity extends AppCompatActivity {
+
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_start_screen);
+        //moves to main activity on click
+        Button enterName = (Button) findViewById(R.id.btn_Enter);
+        enterName.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View view) {
+
+                Intent Start = new Intent(StartScreenActivity.this, MainActivity.class);
+
+                startActivity(Start);
+            }
+
+
+        });
+
     }
+
+
 }
