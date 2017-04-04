37b3cd31f9408b1e2514716913aefca42d09a582
David Long <david.c.long3@gmail.com>
2017-04-04 13:48:33 -0400

    added imageview, text, and list view to the stats page. References stats page still using an intent and new activity.

--- app/src/main/java/trace/traceapp/StatsActivity.java
+++ app/src/main/java/trace/traceapp/StatsActivity.java
@@ -2,6 +2,10 @@ package trace.traceapp;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+
+import java.util.ArrayList;
 
 public class StatsActivity extends AppCompatActivity {
 
@@ -9,5 +13,28 @@ public class StatsActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_stats);
+        myListView = (ListView) findViewById(R.id.LocationList);
+        final ArrayList<String> names = new ArrayList<>();
     }
+
+    /*private String[] name = {
+            "Jeremy",
+            "Drew",
+            "Vinny",
+            "Alan",
+            "Dave"
+    };
+
+    ArrayAdapter<String> nameAdapter = new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, name);
+
+    ListView listView = (ListView) findViewById(R.id.LocationList);
+    listView.setAdapter(itemsAdapter);
+*/
+    private ListView myListView;
+
+
+
+
+
 }
+
