169edb857adf2031a05c2a6742b1ea90ee58aa3b
David Long <david.c.long3@gmail.com>
2017-04-06 13:00:49 -0400

    Got the adapter working and reading serialized strings from a json file in an assets folder I created under main. It displays only the title names as of right now

--- app/src/main/java/trace/traceapp/Location.java
+++ app/src/main/java/trace/traceapp/Location.java
@@ -4,11 +4,15 @@ import android.content.Context;
 
 import org.json.JSONArray;
 import org.json.JSONObject;
+import org.json.JSONException;
 
+import java.io.InputStream;
 import java.util.ArrayList;
 
 /**
  * Created by david on 4/4/2017.
+ * code was found and used through a tutorial at
+ * https://www.raywenderlich.com/124438/android-listview-tutorial
  */
 
 public class Location {
@@ -19,19 +23,53 @@ public class Location {
     public float totalDistance;
     public String mostVisited;
 
-   /* public static ArrayList<Location> getLocationsFromFile(String filename, Context context)
+   public static ArrayList<Location> getLocationsFromFile(String filename, Context context)
     {
         final ArrayList<Location> locationList = new ArrayList<>();
 
         try {
 
+           // String jsonString = loadJsonFromAsset("locations.json", context);
             String jsonString = loadJsonFromAsset("locations.json", context);
             JSONObject json = new JSONObject(jsonString);
             JSONArray locations = json.getJSONArray("locations");
 
+            for(int i = 0; i <locations.length(); i++) {
+                Location location = new Location();
+
+                location.title = locations.getJSONObject(i).getString("title");
+                location.statsLabel = locations.getJSONObject(i).getString("statsLabel");
+                location.addressLabel = locations.getJSONObject(i).getString("addressLabel");
+                //location.totalDistance = locations.getJSONObject(i).getFloat("totalDistance");
+
+                locationList.add(location);
+            }
+        } catch (JSONException e) {
+            e.printStackTrace();
         }
 
-        return 0;
-    } */
+        return locationList;
+    }
+
+    private static String loadJsonFromAsset(String filename, Context context)
+    {
+        String json = null;
+
+        try {
+            InputStream is = context.getAssets().open(filename);
+            int size = is.available();
+            byte[] buffer = new byte[size];
+            is.read(buffer);
+            is.close();
+            json = new String(buffer, "UTF-8");
+        }
+        catch (java.io.IOException ex) {
+            ex.printStackTrace();
+            return null;
+        }
+
+        return json;
+    }
+
 
 }
--- app/src/main/java/trace/traceapp/StatsActivity.java
+++ app/src/main/java/trace/traceapp/StatsActivity.java
@@ -9,28 +9,37 @@ import java.util.ArrayList;
 
 public class StatsActivity extends AppCompatActivity {
 
+    private ListView locListView;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_stats);
-        myListView = (ListView) findViewById(R.id.LocationList);
-        final ArrayList<String> names = new ArrayList<>();
+        locListView = (ListView) findViewById(R.id.LocationList);
+        // 1 loads a list of Recipe objects from a JSON asset in the app
+        //this starter project contains a Recipe class that contains all the information
+        //about the recipes to be displayed.
+        final ArrayList<Location> locationList = Location.getLocationsFromFile("locations.json", this);
+        // 2 creates an array of strings that will contain the text to be displayed.
+        String[] listItems = new String[locationList.size()];
+
+        // 3 Populates the listView with the titles of the recipes lodaed in section one.
+        for (int i =0; i< locationList.size(); i++)
+        {
+            Location location = locationList.get(i);
+            listItems[i] = location.title;
+            //listItems[i] = location.addressLabel;
+        }
+
+        // 4 creates and sets a simpl adapter for the listView.
+        //it takes in the current context, a layout file what each row should look like
+        // and the data that will populate the list as arguments.
+        ArrayAdapter adapter = new ArrayAdapter(this, android.R.layout.simple_list_item_1, listItems);
+        locListView.setAdapter(adapter);
     }
 
-    /*private String[] name = {
-            "Jeremy",
-            "Drew",
-            "Vinny",
-            "Alan",
-            "Dave"
-    };
+    //An adapter loads information to be displayed from a data source, then inserts
+    //the views into the listView.
 
-    ArrayAdapter<String> nameAdapter = new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, name);
-
-    ListView listView = (ListView) findViewById(R.id.LocationList);
-    listView.setAdapter(itemsAdapter);
-*/
-    private ListView myListView;
 
 
 
